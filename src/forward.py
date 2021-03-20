34d10b212c867c642dcc1d48b20e8f46a90572e4
942980741 <942980741@qq.com>
2021-03-20 17:01:55 +0800

    u

--- src/forward.py
+++ src/forward.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
             try:
                 forward(session)
             except Exception as e:
-                logger.error(e)
+                logger.exception(e)
