3ca3fe07a79b95050553a4062d7378eb7024f8e5
942980741 <942980741@qq.com>
2021-04-20 12:36:05 +0800

    解决notes收到的附件名称变成.dat后缀问题

--- src/mail.py
+++ src/mail.py
@@ -27,8 +27,9 @@ class Email:
         for attach in attaches:
             file = open(attach, 'rb')
             payload = MIMEApplication(file.read())
+            payload.add_header('Content-Type','application/octet-stream')
             payload.add_header('Content-Disposition', 'attachment',
-                               filename=os.path.basename(file.name))
+                               filename=Header(os.path.basename(file.name), 'utf-8').encode())
             message.attach(payload)
 
         smtpObj = smtplib.SMTP_SSL(self.smtpserver)
@@ -36,3 +37,11 @@ class Email:
         smtpObj.login(self.username, self.password)
         smtpObj.sendmail(self.sender, receivers, message.as_string())
         smtpObj.quit()
+
+if __name__ == '__main__':
+    email_sender = 'tel15258826527@sina.com'
+    email_password = '337413c8324ffb81'
+    email_smtp_sever = 'smtp.sina.com'
+    m = Email(email_sender, email_smtp_sever, email_sender, email_password)
+    m.send(['xulongzhe@uniview.com'], '2021-04-19 22:38 收到 杭州Java正式 简历1份', '2021-04-19 22:38 收到 杭州Java正式 简历1份', ['/home/xulongzhe/下载/滴滴电子发票 (2).pdf'])
+    
